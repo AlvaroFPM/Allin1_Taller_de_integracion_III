e93c46d5d447221f80fa0eca0c59fd76cece9c53
gustavovlnz <gvalenzuela2023@alu.uct.cl>
2026-09-01 02:49:03 -0400

    feat(#63): registra interceptor global en el gateway

--- backend-microservices/apps/backend-microservices/src/main.ts
+++ backend-microservices/apps/backend-microservices/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TransformInterceptor } from '@app/shared';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalInterceptors(new TransformInterceptor());
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
