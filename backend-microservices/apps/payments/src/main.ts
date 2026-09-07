89cd72c2af0ad0d8efa76b769c3bbadb5f86b911
gustavovlnz <gvalenzuela2023@alu.uct.cl>
2026-09-06 22:53:51 -0300

    feat(#65): registra filtro de errores de prisma en todos los microservicios

--- backend-microservices/apps/payments/src/main.ts
+++ backend-microservices/apps/payments/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { PaymentsModule } from './payments.module';
+import { PrismaExceptionFilter } from '@app/shared';
 
 async function bootstrap() {
   const app = await NestFactory.create(PaymentsModule);
+  app.useGlobalFilters(new PrismaExceptionFilter());
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap().catch(console.error);
--- backend-microservices/apps/search/src/main.ts
+++ backend-microservices/apps/search/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { SearchModule } from './search.module';
+import { PrismaExceptionFilter } from '@app/shared';
 
 async function bootstrap() {
   const app = await NestFactory.create(SearchModule);
+  app.useGlobalFilters(new PrismaExceptionFilter());
   await app.listen(process.env.port ?? 3000);
 }
 bootstrap().catch(console.error);
