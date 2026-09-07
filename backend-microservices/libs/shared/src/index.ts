7798361cc8b252c33e6a1c779faf5ddfad45e91b
gustavovlnz <gvalenzuela2023@alu.uct.cl>
2026-09-06 22:52:07 -0300

    fix(#68): agrega exportacion faltante de upload.service en index.ts

--- backend-microservices/libs/shared/src/index.ts
+++ backend-microservices/libs/shared/src/index.ts
@@ -1,6 +1,6 @@
 export * from './shared.module';
 export * from './shared.service';
 export * from './prisma/base-prisma.service';
-
-
+export * from './filters/prisma-exception.filter';
 export * from './filters/http-exception.filter';
+export * from './upload/upload.service';
