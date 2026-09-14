ea09923b790adaa806a1cdf9eecff3f5380cd8ed
PINILLA MARTÍNEZ ÁLVARO FELIPE <apinilla2024@alu.uct.cl>
2026-09-14 20:24:54 -0300

    feat(#1): agrega contrato auth.proto, configuracion de protoc y genera codigo base gRPC

---
+++ backend-go/generate.ps1
@@ -0,0 +1,19 @@
+$PROTO_DIR = "api\proto"
+$OUT_DIR = "internal\api\pb"
+
+Write-Host "Generando código gRPC y Gateway..."
+
+if (-Not (Test-Path -Path $OUT_DIR)) {
+    New-Item -ItemType Directory -Path $OUT_DIR | Out-Null
+}
+if (-Not (Test-Path -Path "third_party\google\api")) {
+    New-Item -ItemType Directory -Path "third_party\google\api" -Force | Out-Null
+}
+
+# Descargar dependencias de google/api (annotations.proto y http.proto) para el grpc-gateway
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/googleapis/googleapis/master/google/api/annotations.proto" -OutFile "third_party\google\api\annotations.proto"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/googleapis/googleapis/master/google/api/http.proto" -OutFile "third_party\google\api\http.proto"
+
+protoc -I="api\proto" -I="third_party" --go_out="$OUT_DIR" --go_opt=paths=source_relative --go-grpc_out="$OUT_DIR" --go-grpc_opt=paths=source_relative --grpc-gateway_out="$OUT_DIR" --grpc-gateway_opt=paths=source_relative "api\proto\auth.proto"
+
+Write-Host "Generación completada."
