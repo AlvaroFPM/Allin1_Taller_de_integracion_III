58560d794ba235871846566a56a26f3e4747d5e6
Nicolás Fuentes <nicolas.fuentes2024@alu.uct.cl>
2026-09-25 20:00:31 -0300

    fix(catalog): incluir stubs autogenerados de protobuf para GetCategories
    - Agrega archivos generados en pb/publication requeridos para compilar develop.
    - Resuelve error de compilacion por GetCategoriesRequest indefinido en main.

--- backend-go/internal/api/pb/publication/publication.pb.gw.go
+++ backend-go/internal/api/pb/publication/publication.pb.gw.go
@@ -35,30 +35,24 @@ var (
 	_ = metadata.Join
 )
 
-func request_PublicationService_CreatePublication_0(ctx context.Context, marshaler runtime.Marshaler, client PublicationServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+func request_PublicationService_GetCategories_0(ctx context.Context, marshaler runtime.Marshaler, client PublicationServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var (
-		protoReq CreatePublicationRequest
+		protoReq GetCategoriesRequest
 		metadata runtime.ServerMetadata
 	)
-	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && !errors.Is(err, io.EOF) {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
 	if req.Body != nil {
 		_, _ = io.Copy(io.Discard, req.Body)
 	}
-	msg, err := client.CreatePublication(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	msg, err := client.GetCategories(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 }
 
-func local_request_PublicationService_CreatePublication_0(ctx context.Context, marshaler runtime.Marshaler, server PublicationServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+func local_request_PublicationService_GetCategories_0(ctx context.Context, marshaler runtime.Marshaler, server PublicationServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var (
-		protoReq CreatePublicationRequest
+		protoReq GetCategoriesRequest
 		metadata runtime.ServerMetadata
 	)
-	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && !errors.Is(err, io.EOF) {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	msg, err := server.CreatePublication(ctx, &protoReq)
+	msg, err := server.GetCategories(ctx, &protoReq)
 	return msg, metadata, err
 }
 
@@ -142,25 +136,25 @@ func local_request_PublicationService_ListPublications_0(ctx context.Context, ma
 // Note that using this registration option will cause many gRPC library features to stop working. Consider using RegisterPublicationServiceHandlerFromEndpoint instead.
 // GRPC interceptors will not work for this type of registration. To use interceptors, you must use the "runtime.WithMiddlewares" option in the "runtime.NewServeMux" call.
 func RegisterPublicationServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server PublicationServiceServer) error {
-	mux.Handle(http.MethodPost, pattern_PublicationService_CreatePublication_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle(http.MethodGet, pattern_PublicationService_GetCategories_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
 		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		annotatedContext, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/publication.PublicationService/CreatePublication", runtime.WithHTTPPathPattern("/v1/publications"))
+		annotatedContext, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/publication.PublicationService/GetCategories", runtime.WithHTTPPathPattern("/v1/categories"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_PublicationService_CreatePublication_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_PublicationService_GetCategories_0(annotatedContext, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
 		if err != nil {
 			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		forward_PublicationService_CreatePublication_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_PublicationService_GetCategories_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
 	mux.Handle(http.MethodGet, pattern_PublicationService_GetPublication_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
@@ -242,22 +236,22 @@ func RegisterPublicationServiceHandler(ctx context.Context, mux *runtime.ServeMu
 // doesn't go through the normal gRPC flow (creating a gRPC client etc.) then it will be up to the passed in
 // "PublicationServiceClient" to call the correct interceptors. This client ignores the HTTP middlewares.
 func RegisterPublicationServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux, client PublicationServiceClient) error {
-	mux.Handle(http.MethodPost, pattern_PublicationService_CreatePublication_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle(http.MethodGet, pattern_PublicationService_GetCategories_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		annotatedContext, err := runtime.AnnotateContext(ctx, mux, req, "/publication.PublicationService/CreatePublication", runtime.WithHTTPPathPattern("/v1/publications"))
+		annotatedContext, err := runtime.AnnotateContext(ctx, mux, req, "/publication.PublicationService/GetCategories", runtime.WithHTTPPathPattern("/v1/categories"))
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_PublicationService_CreatePublication_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_PublicationService_GetCategories_0(annotatedContext, inboundMarshaler, client, req, pathParams)
 		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
 		if err != nil {
 			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		forward_PublicationService_CreatePublication_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_PublicationService_GetCategories_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 	})
 	mux.Handle(http.MethodGet, pattern_PublicationService_GetPublication_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
@@ -297,13 +291,13 @@ func RegisterPublicationServiceHandlerClient(ctx context.Context, mux *runtime.S
 }
 
 var (
-	pattern_PublicationService_CreatePublication_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "publications"}, ""))
-	pattern_PublicationService_GetPublication_0    = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "publications", "id_publicacion"}, ""))
-	pattern_PublicationService_ListPublications_0  = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "publications"}, ""))
+	pattern_PublicationService_GetCategories_0    = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "categories"}, ""))
+	pattern_PublicationService_GetPublication_0   = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"v1", "publications", "id_publicacion"}, ""))
+	pattern_PublicationService_ListPublications_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "publications"}, ""))
 )
 
 var (
-	forward_PublicationService_CreatePublication_0 = runtime.ForwardResponseMessage
-	forward_PublicationService_GetPublication_0    = runtime.ForwardResponseMessage
-	forward_PublicationService_ListPublications_0  = runtime.ForwardResponseMessage
+	forward_PublicationService_GetCategories_0    = runtime.ForwardResponseMessage
+	forward_PublicationService_GetPublication_0   = runtime.ForwardResponseMessage
+	forward_PublicationService_ListPublications_0 = runtime.ForwardResponseMessage
 )
